f1f5575ea00f1f065c0239bcd0eb2ac978fc44bf
hshikur <hshikur@users.noreply.github.com>
2017-12-28 12:13:01 +0200

    Add files via upload

---
+++ food.py
@@ -0,0 +1,14 @@
+def count_char(text, char):
+	count = 0
+	for c in text:
+		if c == char:
+			count += 1
+	return count
+
+
+
+filename = raw_input("Enter a filename:")
+with open(filename) as f:
+	text = f.read()
+
+print(count_char(text, "x"))
